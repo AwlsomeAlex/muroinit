b5ad9ed030785550a09183113d59f69333236cd6
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-27 23:08:46 -0400

    Created Common Library

--- src_old/etc/20_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo -e "\e[1;32m(Pass) \e[0mSwitch Root to OverlayFS Successful."
-echo -e "\e[1;94m(****) \e[0mExecuting /sbin/init as PID 1..."
-exec /sbin/init
-
-echo -e "\e[1;31m(Fail) \e[0mInit Script Failed."
